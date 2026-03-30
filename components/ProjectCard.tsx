6ab8c5c5843cbae5e8526007d267bb7b0b2a5668
João Victor <jv.joaocoelho@gmail.com>
2026-03-30 09:20:26 -0300

    wip: projects carousel

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,11 +4,22 @@ import {Button} from "./ui/button";
 import {ExternalLink, Info} from "lucide-react";
 import Image from "next/image";
 
-export const ProjectCard = () => {
+export type ProjectData = {
+  image: string;
+  about: string;
+  github: string | null;
+  deploy: string | null;
+};
+
+export type ProjectCardProps = {
+  data: ProjectData;
+};
+
+export const ProjectCard = ({data}: ProjectCardProps) => {
   return (
     <div className="w-full">
       <div className="bg-white relative h-full w-full drop-shadow-xl rounded-2xl overflow-hidden">
-        <Image src={"/my-finances.png"} width={800} height={480} alt="" />
+        <Image src={data.image} width={800} height={480} alt="" />
         <div className="flex flex-col justify-end gap-4 p-8 absolute bottom-0">
           <div className="flex items-center gap-2">
             <Button size="sm" variant="primary" onClick={() => {}} className="gap-1.5">
--- components/ProjectsCarousel.tsx
+++ components/ProjectsCarousel.tsx
@@ -1,30 +1,62 @@
-import {useState} from "react";
-import {ProjectCard} from "./ProjectCard";
+import {useEffect, useRef, useState} from "react";
+import {ProjectCard, ProjectData} from "./ProjectCard";
 import clsx from "clsx";
-
-export type ProjectData = {
-  image: string;
-  about: string;
-  github: string | null;
-  deploy: string | null;
-};
 
 export type ProjectsCarouselProps = {
   projectsData: ProjectData[];
 };
 
 export const ProjectsCarousel = ({projectsData}: ProjectsCarouselProps) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(2);
+  const dragStartRef = useRef([0, 0]);
+
+  const rotateAngle = 22.5;
+
+  useEffect(() => {
+    document.addEventListener("dragover", (e) => e.preventDefault());
+  }, []);
 
   return (
-    <div className="w-full whitespace-nowrap overflow-auto no-scrollbar relative">
-      {projectsData.map((projectData, i) => {
-        return (
-          <div className={clsx(i === currentIndex ? "w-200 h-120" : "w-160 h-112")}>
-            <ProjectCard key={projectData.image + i} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div
+        className="w-full whitespace-nowrap overflow-hidden relative h-140 perspective-distant touch-none"
+        onDragStart={(e) => {
+          dragStartRef.current = [e.clientX, currentIndex];
+        }}
+        onDrag={(e) => {
+          if (!e.clientX) return;
+          const offset = dragStartRef.current[0] - e.clientX;
+          const ci = dragStartRef.current[1];
+          setCurrentIndex(Math.max(0, Math.min(projectsData.length - 1, ci + offset / 450)));
+        }}
+        onTouchStart={(e) => {
+          dragStartRef.current = [e.targetTouches[0].clientX, currentIndex];
+        }}
+        onTouchMove={(e) => {
+          if (!e.targetTouches[0].clientX) return;
+          const offset = dragStartRef.current[0] - e.targetTouches[0].clientX;
+          const ci = dragStartRef.current[1];
+          setCurrentIndex(Math.max(0, Math.min(projectsData.length - 1, ci + offset / 450)));
+        }}
+      >
+        {projectsData.map((projectData, i) => {
+          return (
+            <div
+              style={{
+                top: "50%",
+                left: `calc(50% + ${(i - currentIndex) * (550 * Math.cos((i - currentIndex) * rotateAngle * (Math.PI / 180)))}px)`,
+                width: `${(1 - Math.abs(i - currentIndex) * 0.2) * 800}px`,
+                height: `${(1 - Math.abs(i - currentIndex) * 0.2) * 480}px`,
+                zIndex: `${Math.max(0, 10 - Math.abs(Math.round(i - currentIndex)))}`,
+                rotate: `y ${(i - currentIndex) * rotateAngle}deg`,
+              }}
+              className={clsx("absolute -translate-x-1/2 top-1/2 -translate-y-1/2")}
+            >
+              <ProjectCard key={projectData.image + i} data={projectData} />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };

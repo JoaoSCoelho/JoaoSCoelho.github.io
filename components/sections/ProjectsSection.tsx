95b7e10aedaf635de9960629ea5fa1a2e7c28379
João Victor <jv.joaocoelho@gmail.com>
2026-04-01 09:30:12 -0300

    feat: adding new project

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -15,42 +15,49 @@ export const ProjectsSection = () => {
           <ProjectsCarousel
             projectsData={[
               {
-                title: "My Finances (Front-End)",
-                image: "/my-finances-front.png",
+                title: "My Finances",
+                image: "/my-finances.png",
                 about: (
-                  <div>
-                    <p className="whitespace-normal">
+                  <div className="whitespace-normal">
+                    <p>
                       O projeto <strong>My Finances (Front-End)</strong> é um gerenciador de despesas pessoais
                       feito em Next.JS com TypeScript.
                       <br />
                       A proposta foi fazer um projeto simples envolvendo front-end e back-end, deixá-lo online
                       e com uma documentação bem legível.
                       <img
-                        className="mt-3"
+                        className="my-3"
                         src="https://github.com/JoaoSCoelho/my-finances-frontend/blob/main/resources/usage-gif.gif?raw=true"
                         alt="GIF de uso do MyFinances"
                       />
                     </p>
-                  </div>
-                ),
-                deploy: "https://my-finances-frontend.vercel.app/",
-                github: "https://github.com/JoaoSCoelho/my-finances-frontend",
-              },
-              {
-                title: "My Finances (Back-End)",
-                image: "/my-finances-back.png",
-                about: (
-                  <div>
-                    <p className="whitespace-normal">
-                      O projeto <strong>My Finances (Back-End)</strong> é a API de gerenciamento de despesas
-                      pessoais feito em Express com TypeScript, usando ferramentas como BCrypt, Json Web Token
-                      e Jest, princípios do SOLID e Clean Architecture e banco de dados NoSQL MongoDB.
-                      <br />A proposta foi fazer um projeto simples envolvendo front-end e back-end, deixá-lo
-                      online e com uma documentação bem legível.
+                    <p>
+                      O projeto conta com um Back-End feito em Express com TypeScript, usando ferramentas como
+                      BCrypt, Json Web Token e Jest, princípios do SOLID e Clean Architecture e banco de dados
+                      NoSQL MongoDB.
                     </p>
-                    <figure>
+                    <p>
+                      O Back-End pode ser acessado por aqui:{" "}
+                      <a
+                        className="text-blue-100 underline cursor-pointer"
+                        href="https://api.myfinances.joaoscoelho.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        api.myfinances.joaoscoelho.com
+                      </a>
+                      e o código fonte encontra-se disponível no GitHub em:
+                      <a
+                        className="text-blue-100 underline cursor-pointer"
+                        href="https://github.com/JoaoSCoelho/my-finances-backend"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        https://github.com/JoaoSCoelho/my-finances-backend
+                      </a>
+                    </p>
+                    <figure className="my-3">
                       <img
-                        className="mt-3"
                         src="https://github.com/JoaoSCoelho/my-finances-backend/raw/main/resources/backend-workflow.png"
                         alt="Workflow backend"
                       />
@@ -58,8 +65,8 @@ export const ProjectsSection = () => {
                     </figure>
                   </div>
                 ),
-                deploy: "https://my-finances-backend.onrender.com",
-                github: "https://github.com/JoaoSCoelho/my-finances-backend",
+                deploy: "https://myfinances.joaoscoelho.com",
+                github: "https://github.com/JoaoSCoelho/my-finances-frontend",
               },
               {
                 title: "Coffee",
@@ -78,6 +85,36 @@ export const ProjectsSection = () => {
                 ),
                 deploy: null,
                 github: "https://github.com/MyRepoArchive/Coffee",
+              },
+              {
+                title: "IFBA Dashboard",
+                image: "/if-dash.png",
+                about: (
+                  <div className="whitespace-normal">
+                    <p>
+                      O <strong>IFBA Dashboard</strong> (IF Dash) foi um site criado inteiramente com Next.JS
+                      e Firebase para suprir uma demanda pessoal e de parte da minha turma do colégio no
+                      ensino médio, durante o período de aulas remotas. Este projeto foi feito em
+                      aproximadamente 1 semana utilizando-se de TypeScript e Next Apis para acelerar o
+                      processo de desenvolvimento.
+                    </p>
+                    <figure className="my-3">
+                      <img
+                        src="https://github.com/JoaoSCoelho/ifba_dashboard/blob/main/resources/usage-example.gif?raw=true"
+                        alt="Demo IF Dash"
+                      />
+                      <legend>Demonstração do IF Dash</legend>
+                    </figure>
+                    <p>
+                      <span className="text-orange-300">
+                        Atualmente devido a atualizações do Next APIs, o backend encontra-se offline, não
+                        sendo possível se autenticar no sistema, mas a página inicial segue viva!
+                      </span>
+                    </p>
+                  </div>
+                ),
+                deploy: "https://ifdash.joaoscoelho.com",
+                github: "https://github.com/JoaoSCoelho/ifba_dashboard",
               },
             ]}
           />
